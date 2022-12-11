b928476add6de97979947705a816ec4492bb98a1
Carlos <charlie.oktopous@gmail.com>
2022-12-11 19:25:16 +0100

    supabase uncommented to test signin and signup

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -3,24 +3,82 @@
   <template>
 
   <div class="container">
-    <h3 class="header-title">Log In to ToDo App</h3>
-    <p class="header-subtitle">Estamos en la ruta de login. Aquí deberíais crear un form con la lógica correspondiente para que este permita al usuario loguearse con su email y su contraseña. Miraros la lógica de SignUp si necesitáis inspiración :)</p>
-    <p>Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
+    <form @submit.prevent="signIn" class="form-sign-in">
+      <div class="form">
+        <div class="form-input">
+          <label class="input-field-label">E-mail</label>
+          <input
+            type="email"
+            class="input-field"
+            placeholder="example@gmail.com"
+            id="email"
+            v-model="email"
+            required
+          />
+        </div>
+        <div class="form-input">
+          <label class="input-field-label">Password</label>
+          <input
+            type="password"
+            class="input-field"
+            placeholder="**********"
+            id="password"
+            v-model="password"
+            required
+          />
+        </div>
+        <button class="button" type="submit">Sign In</button>
+        <p>
+          Have an account?
+          <PersonalRouter
+            :route="route"
+            :buttonText="buttonText"
+            class="sign-up-link"
+          />
+        </p>
+      </div>
+    </form>
+    <div v-show="errorMsg">{{errorMsg}}</div>
   </div>
 
 </template>
 
 <script setup>
 import PersonalRouter from "./PersonalRouter.vue";
+import { useRouter } from "vue-router";
+import { useUserStore } from "../stores/user";
+import { ref, computed } from "vue";
+const redirect = useRouter();
 
 // Route Variables
 const route = "/auth/signup";
 const buttonText = "Sign Up";
 
+// Input Fields
+const email = ref("");
+const password = ref("");
+
+// Error Message
+const errorMsg = ref("");
+
 // Arrow function to Signin user to supaBase
 const signIn = async () => {
-  try {} catch (error) {}
+  try {
+    // calls the user store and send the users info to backend to logIn
+    await useUserStore().signIn(email.value, password.value);
+    // redirects user to the homeView
+    redirect.push({ path: "/home" });
+  } catch (error) {
+    // displays error message
+    errorMsg.value = error.message;
+    // hides error message
+    setTimeout(() => {
+      errorMsg.value = null;
+    }, 5000);
+  }
+  errorMsg.value = "error";
 };
+
 </script>
 
 <style></style>
