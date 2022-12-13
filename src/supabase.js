8514fcd354568ab14edf92915753e58de9050b79
Carlos <charlie.oktopous@gmail.com>
2022-12-13 23:28:44 +0100

    clear console.logs

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-console.log("🚀 ~ file: supabase.js:4 ~ supabaseUrl", supabaseUrl)
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log("🚀 ~ file: supabase.js:6 ~ supabaseAnonKey", supabaseAnonKey)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-console.log("🚀 ~ file: supabase.js:8 ~ supabase", supabase)
