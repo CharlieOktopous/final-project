f693325bb90cfadbd2a4799d377f7f7610ad9e96
Carlos <charlie.oktopous@gmail.com>
2022-12-15 00:26:44 +0100

    navbar adapted to mobile

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,58 @@
 <template>
-  <nav
-    class="navbar px-16 w-full flex justify-between items-center bg-blue-500 text-white h-16"
-  >
-    <div class="flex gap-10">
-      <router-link to="/" class="router-link font-semibold">Task Manager</router-link>
-      <router-link to="/account" class="router-link font-semibold">Your Account</router-link>
+  <nav class="navbar w-full">
+    <div
+      class="desktop-menu px-16 w-full flex justify-between items-center text-white h-16"
+    >
+      <div class="flex gap-10">
+        <router-link to="/" class="router-link font-semibold"
+          >Task Manager</router-link
+        >
+        <router-link to="/account" class="router-link font-semibold"
+          >Your Account</router-link
+        >
+      </div>
+      <div class="flex items-center gap-20">
+        <p class="flex gap-4" v-if="useUserStore().user">
+          <span> Welcome</span>
+          <span class="font-bold">{{ useUserStore().user.email }}</span>
+        </p>
+        <button @click="signOut" class="button button-log-out">
+          <span class="mb-1">Logout</span>
+        </button>
+      </div>
     </div>
-    <div class="flex items-center gap-20">
-      <p class="flex gap-4" v-if="useUserStore().user">
-        <span> Welcome</span>
-        <span class="font-bold">{{ useUserStore().user.email }}</span>
-      </p>
-      <button @click="signOut" class="button button-log-out">
-        <span class="mb-1">Logout</span>
-      </button>
+
+    <div class="burger-menu">
+      <div class="hamburger-icon" @click="actionShowBurger">
+        <svg
+          class="h-8 w-8 midnight-blue"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 10h16M4 14h16M4 18h16"
+          />
+        </svg>
+      </div>
+      <div class="mobile-navbar" v-show="showBurger">
+        <div class="mobile-link">
+          <router-link to="/">Task Manager</router-link>
+        </div>
+
+        <div class="mobile-link">
+          <router-link to="/account">Your Account</router-link>
+        </div>
+
+        <div class="mobile-logout">
+          <button @click="signOut" class="font-bold">
+            Log out
+          </button>
+        </div>
+      </div>
     </div>
     <!-- Aplication hamburguer -->
     <!-- <button @click="toggle" class="toggle"> </button>
@@ -45,6 +84,11 @@ import { ref } from "vue";
 //constant to save a variable that will hold the use router method
 const route = "/";
 const buttonText = "Todo app";
+
+const showBurger = ref(false);
+const actionShowBurger = () => {
+  showBurger.value = !showBurger.value;
+};
 
 // constant to save a variable that will get the user from store with a computed function imported from vue
 // const getUser = computed(() => useUserStore().user);
